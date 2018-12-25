1d9b779d9f791ed227abbb27b21511fd57b01167
Hakob Petrosyan <hpetrosyan@questrade.com>
2018-12-25 14:46:02 -0500

    a

--- .vscode/tt.py
+++ .vscode/tt.py
@@ -4,20 +4,16 @@ from scipy import stats
 from re import sub
 import misc_ops
 import requests
-from yahoo_finance import Share
+
 
 mo = misc_ops.MiscOps()
 
-print(mo.get_date_from_string('2018-01-14'))
 
-# yahoo = Share('YHOO')
-# print (yahoo.get_open())
+resp = requests.get('https://api.github.com/users/hadley/orgs')
 
-# # resp = requests.get('https://api.github.com/users/hadley/orgs')
-# resp = requests.get('link')
-# if resp.status_code != 200:
-#     # This means something went wrong.
-#     raise ApiError('GET /tasks/ {}'.format(resp.status_code))
-# for todo_item in resp.json():
-#     # print('{} {}'.format(todo_item['id'], todo_item['login']))
-#     print(todo_item)
+if resp.status_code != 200:
+    # This means something went wrong.
+    raise ApiError('GET /tasks/ {}'.format(resp.status_code))
+for todo_item in resp.json():
+    print('{} {}'.format(todo_item['id'], todo_item['login']))
+    # print(todo_item)
