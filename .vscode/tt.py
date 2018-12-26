6bc2b3f5aa0fb51bbd0d56ae85fb989fa6333e0e
Hakob Petrosyan <hpetrosyan@questrade.com>
2018-12-25 21:09:32 -0500

    d

--- .vscode/tt.py
+++ .vscode/tt.py
@@ -9,11 +9,19 @@ import requests
 mo = misc_ops.MiscOps()
 
 
-resp = requests.get('https://api.github.com/users/hadley/orgs')
+resp = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo')
 
 if resp.status_code != 200:
     # This means something went wrong.
     raise ApiError('GET /tasks/ {}'.format(resp.status_code))
-for todo_item in resp.json():
-    print('{} {}'.format(todo_item['id'], todo_item['login']))
+resp_json = resp.json() # ['Time Series (Daily)']
+# for todo_item in resp.json():
+    # print('{} {}'.format(todo_item['id'], todo_item['login']))
     # print(todo_item)
+
+resp_metadata = resp_json['Meta Data']
+resp_time_series = resp_json['Time Series (Daily)']
+
+
+for key, value in resp_time_series.items():
+    print(key, value)
